363d3d2de221513cb52b0b578dd5782614969e3f
Fabio Ewerton <fabioew89@gmail.com>
2017-08-13 01:50:11 -0400

    desafio 02 - Data Formatada

---
+++ desafios/desafio02/dataFormatada.py
@@ -0,0 +1,6 @@
+dia = input("Oi, qual o dia do seu aniversário ? ")
+mes = input("e o mês ? ")
+ano = input("e o ano ? ")
+
+print("Poxa... que bacana, então seu aniversário é dia",dia, "de",mes,"de",ano,"!")
+
