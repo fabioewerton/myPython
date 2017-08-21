6174617e81ad283ebf741fadcd39b73db6c5245f
Fábio Ewerton <fabioew89@gmail.com>
2017-08-21 17:46:26 -0400

    renew ex009_2.0

--- ExerciciosPython/ex009.py
+++ ExerciciosPython/ex009.py
@@ -1,45 +1,15 @@
 #Exercício Python 009: Faça um programa que leia um número Inteiro qualquer e mostre na tela a sua tabuada.
+n = int(input('Digite um número para ver sua tabuada: '))
 
-n = int(input('Digite um número: '))
-t1 = n * 1
-t2 = n * 2
-t3 = n * 3
-t4 = n * 4
-t5 = n * 5
-t6 = n * 6
-t7 = n * 7
-t8 = n * 8
-t9 = n * 9
-t10 = n * 10
-
-line = '-' * 20
-print(line)
-print('A tabuada de {} é:'
-      '\n{} x 1 = {}'
-      '\n{} x 2 = {}'
-      '\n{} x 3 = {}'
-      '\n{} x 4 = {}'
-      '\n{} x 5 = {}'
-      '\n{} x 6 = {}'
-      '\n{} x 7 = {}'
-      '\n{} x 8 = {}'
-      '\n{} x 9 = {}'
-      '\n{} x 10 = {}'
-      .format(n, n, t1, n, t2, n, t3, n, t4, n, t5, n, t6, n, t7, n, t8, n, t9, n, t10))
-print(line)
-
-print('')
-print(line)
-print('A tabuada de {} é:'
-      '\n{} x 1 = {}'
-      '\n{} x 2 = {}'
-      '\n{} x 3 = {}'
-      '\n{} x 4 = {}'
-      '\n{} x 5 = {}'
-      '\n{} x 6 = {}'
-      '\n{} x 7 = {}'
-      '\n{} x 8 = {}'
-      '\n{} x 9 = {}'
-      '\n{} x 10 = {}'
-      .format(n, n, t1, n, t2, n, t3, n, t4, n, t5, n, t6, n, t7, n, t8, n, t9, n, t10))
-print(line)
+print('='*20)
+print('{} x {:2} = {}'.format(n, 1, n*1))
+print('{} x {:2} = {}'.format(n, 2, n*2))
+print('{} x {:2} = {}'.format(n, 3, n*3))
+print('{} x {:2} = {}'.format(n, 4, n*4))
+print('{} x {:2} = {}'.format(n, 5, n*5))
+print('{} x {:2} = {}'.format(n, 6, n*6))
+print('{} x {:2} = {}'.format(n, 7, n*7))
+print('{} x {:2} = {}'.format(n, 8, n*8))
+print('{} x {:2} = {}'.format(n, 9, n*9))
+print('{} x {:2} = {}'.format(n, 10, n*10))
+print('='*20)
