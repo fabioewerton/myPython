d27b68f870f8044b29dcf3227e33deadde306ffe
Fábio Ewerton <fabioew89@gmail.com>
2017-08-21 17:49:06 -0400

    add .idea/workspace and modify ex009_2.0

--- ExerciciosPython/ex009_2.0.py
+++ ExerciciosPython/ex009_2.0.py
@@ -27,19 +27,3 @@ print('A tabuada de {} é:'
       '\n{} x 10 = {}'
       .format(n, n, t1, n, t2, n, t3, n, t4, n, t5, n, t6, n, t7, n, t8, n, t9, n, t10))
 print(line)
-
-print('')
-print(line)
-print('A tabuada de {} é:'
-      '\n{} x 1 = {}'
-      '\n{} x 2 = {}'
-      '\n{} x 3 = {}'
-      '\n{} x 4 = {}'
-      '\n{} x 5 = {}'
-      '\n{} x 6 = {}'
-      '\n{} x 7 = {}'
-      '\n{} x 8 = {}'
-      '\n{} x 9 = {}'
-      '\n{} x 10 = {}'
-      .format(n, n, t1, n, t2, n, t3, n, t4, n, t5, n, t6, n, t7, n, t8, n, t9, n, t10))
-print(line)
