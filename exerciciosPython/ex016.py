87e870592b6906ba0254a5c8e4ef19dd8de61775
Fabio Ewerton <fabioew89@gmail.com>
2017-08-25 22:30:03 -0400

    ex016 ok

---
+++ exerciciosPython/ex016.py
@@ -0,0 +1,9 @@
+#Exercício Python 016: Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.
+"""from math import trunc
+
+num = float(input('Digite um número: '))
+print('Seu número foi {} e a parte inteira dele é {}'.format(num, trunc(num)))"""
+
+#Dica do Guanabara: show
+num = float(input('Digite um número: '))
+print('Seu número foi {} e a parte inteira dele é {}'.format(num, int(num)))
