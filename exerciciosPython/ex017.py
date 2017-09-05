faddafcf4429801ed161336281fdf52aed21db12
Fábio Ewerton <fabioew89@gmail.com>
2017-09-05 01:00:36 -0400

    Teste da nova máquina ubuntu-home

--- exerciciosPython/ex017.py
+++ exerciciosPython/ex017.py
@@ -8,3 +8,4 @@ ca = float(input('comprimento do cateto adjacente: '))
 hi = hypot(co, ca)
 
 print('Se meu cateto oposto é {} e meu cateto adjacente é {}, então a minha hipotenusa é {:.2f}'.format(co, ca, hi))
+print('Se meu cateto oposto é {} e meu cateto adjacente é {}, então a minha hipotenusa é {:.2f}'.format(co, ca, hi))
